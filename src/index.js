ab9e9ddca10c2fb4734910f0f43cc0df3c954726
Viveks610 <vivek.shrirame123@gmail.com>
2023-05-13 19:08:53 +0530

    FIXED

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-
-// import App from "./App";
-
-// ReactDOM.render(<App />, document.getElementById("root"));
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
